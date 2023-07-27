fd6791e34c344353eca2674f9463e52824c68146
Naveen Chandar <naveenchandece@gmail.com>
2023-07-27 08:38:29 +0530

    check how angular loads

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FirstComponent } from './first.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,6 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [FirstComponent]
 })
 export class AppModule { }
