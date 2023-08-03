1876f739392045080f929faa6373ff4fb4ad000b
Naveen Chandar <naveenchandece@gmail.com>
2023-08-03 08:23:47 +0530

    view encapsulation

--- angular-crud/src/app/app.component.ts
+++ angular-crud/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // encapsulation: ViewEncapsulation.None // it applies globally
+  // encapsulation: ViewEncapsulation.Emulated // it applies only to this element (not even to child). This is the default behaviour
+  // encapsulation: ViewEncapsulation.ShadowDom // it applies to this element and children
 })
 export class AppComponent {
   inputText = 'angular-crud';
--- angular-crud/src/app/components/InputOutput/inputoutput.component.ts
+++ angular-crud/src/app/components/InputOutput/inputoutput.component.ts
@@ -3,6 +3,9 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 @Component({
     selector: "app-inputoutput",
     templateUrl: "./inputoutput.component.html",
+    // styles: [
+    //     'p {color: blueviolet;}'
+    // ]
 })
 export class InputOutComponent {
     @Input() serverData!: String
