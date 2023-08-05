16a89291498f0e4629f1ec1f9b17a729e6ae1cb2
Naveen Chandar <naveenchandece@gmail.com>
2023-08-05 13:06:50 +0530

    content child

--- angular-crud/src/app/components/second/second.component.ts
+++ angular-crud/src/app/components/second/second.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
-export class SecondComponent {
-
+export class SecondComponent implements OnInit, AfterContentInit {
+  @ContentChild("labelContent") labelConent!: ElementRef;
+  
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit labelConent",this.labelConent)
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit labelConent",this.labelConent)
+  }
 }
